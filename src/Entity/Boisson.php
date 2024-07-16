0aa7138943541c53fb42e3074372e47a2882b5a9
Thea Blachon <thea.blachon73@gmail.com>
2024-07-16 11:07:01 +0200

    mise à jour de l'entité media et boisson

--- src/Entity/Boisson.php
+++ src/Entity/Boisson.php
@@ -3,23 +3,38 @@
 namespace App\Entity;
 
 use App\Repository\BoissonRepository;
+use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
+use ApiPlatform\Metadata\ApiResource;
+use ApiPlatform\Metadata\GetCollection;
+use ApiPlatform\Metadata\Get;
+use Symfony\Component\Serializer\Attribute\Groups;
+use ApiPlatform\Metadata\ApiFilter;
 
 #[ORM\Entity(repositoryClass: BoissonRepository::class)]
+#[ApiResource(
+    normalizationContext: ['groups' => ['read']],
+    denormalizationContext: ['groups' => ['write']],
+    forceEager: false,
+)]
 class Boisson
 {
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column]
+    #[Groups('read')]
     private ?int $id = null;
 
     #[ORM\Column(length: 100)]
+    #[Groups(['read', 'write'])]
     private ?string $name = null;
 
     #[ORM\Column]
+    #[Groups(['read', 'write'])]
     private ?float $price = null;
 
     #[ORM\OneToOne(inversedBy: 'boisson', cascade: ['persist', 'remove'])]
+    #[Groups(['read', 'write'])]
     private ?Media $photo = null;
 
     public function getId(): ?int
--- src/Entity/Media.php
+++ src/Entity/Media.php
@@ -4,20 +4,76 @@ namespace App\Entity;
 
 use App\Repository\MediaRepository;
 use Doctrine\ORM\Mapping as ORM;
+use ApiPlatform\Metadata\GetCollection;
+use ApiPlatform\Metadata\Get;
+use ApiPlatform\Metadata\Post;
+use ApiPlatform\Metadata\Patch;
+use ApiPlatform\Metadata\Delete;
+use ApiPlatform\OpenApi\Model;
+use ApiPlatform\Metadata\ApiProperty;
+use App\Controller\CreateMediaObjectAction;
+use Symfony\Component\HttpFoundation\File\File;
+use Vich\UploaderBundle\Mapping\Annotation as Vich;
+use ApiPlatform\Metadata\ApiResource;
+use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+use Symfony\Component\Serializer\Attribute\Groups;
+
 
 #[ORM\Entity(repositoryClass: MediaRepository::class)]
+#[Vich\Uploadable]
+#[ApiResource(
+    normalizationContext: ['groups' => ['read']],
+    types: ['https://schema.org/MediaObject'],
+    forceEager: false,
+    operations: [
+        new Get(),
+        new GetCollection(),
+        new Delete(),
+        new Post(
+            controller: CreateMediaObjectAction::class,
+            deserialize: false,
+            validationContext: ['groups' => ['Default', 'write']],
+            openapi: new Model\Operation(
+                requestBody: new Model\RequestBody(
+                    content: new \ArrayObject([
+                        'multipart/form-data' => [
+                            'schema' => [
+                                'type' => 'object',
+                                'properties' => [
+                                    'file' => [
+                                        'type' => 'string',
+                                        'format' => 'binary'
+                                    ]
+                                ]
+                            ]
+                        ]
+                    ])
+                )
+            )
+        ),
+    ]
+)]
+
 class Media
 {
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column]
+    #[Groups('read')]
     private ?int $id = null;
 
     #[ORM\Column(length: 255)]
+    #[Vich\UploadableField(mapping: 'media_object', fileNameProperty: 'filePath')]
+    #[Assert\NotNull(groups: ['write'])]
     private ?string $file = null;
 
     #[ORM\OneToOne(mappedBy: 'photo', cascade: ['persist', 'remove'])]
     private ?Boisson $boisson = null;
+
+    #[ApiProperty(types: ['https://schema.org/contentUrl'])]
+    #[Groups(['read'])]
+    public ?string $contentUrl = null;
 
     public function getId(): ?int
     {
